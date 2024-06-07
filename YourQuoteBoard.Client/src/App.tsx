2977b764811eb6f47cdcb3c41dba9737dee932fe
Domantas1337 <domantas.kakc@gmail.com>
2024-06-07 16:02:19 +0300

    Created the client project

---
+++ YourQuoteBoard.Client/src/App.tsx
@@ -0,0 +1,7 @@
+import Card from "./components/Card"
+
+function App() {
+  return <div><Card></Card></div>
+}
+
+export default App
---
+++ YourQuoteBoard.Client/src/components/Card.tsx
@@ -0,0 +1,14 @@
+ 
+
+function Card(){
+
+    return <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some text</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>  
+}
+
+export default Card;
